4c7a19269df714e748a3e43e25318e09c36091e3
atkinsh <atkinsh@umich.edu>
2022-11-22 18:54:33 -0500

    job0

--- inverted_index/map0.py
+++ inverted_index/map0.py
@@ -1,1 +1,12 @@
+#!/usr/bin/env python3
 """Map 0."""
+
+import sys
+import csv
+import re
+
+csv.field_size_limit(sys.maxsize)
+
+for row in csv.reader(sys.stdin):
+    #outputs doc, 1 for each line
+    print(f"doc\t1")
--- inverted_index/reduce0.py
+++ inverted_index/reduce0.py
@@ -1,1 +1,27 @@
+#!/usr/bin/env python3
 """Reduce 0."""
+
+import sys
+import itertools
+
+# sums and outputs number of lines
+def reduce0(key, group):
+    group = list(group)
+    total = 0
+    for line in group:
+        total += 1
+
+    print(f"{total}")
+
+
+def main():
+    """Divide sorted lines into groups that share a key.
+    Docs: https://docs.python.org/3/library/itertools.html#itertools.groupby
+    """
+    groups = itertools.groupby(sys.stdin, lambda x: x.partition("\t")[0])
+    for key, group in groups:
+        reduce0(key, group)
+
+
+if __name__ == "__main__":
+    main()
